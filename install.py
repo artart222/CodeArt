62f85731e18b2eb8808cc754c80a956e586a0576
artin222 <mobasherartin.icm@gmail.com>
2021-01-11 15:45:30 +0330

    Update install.py
    Add mac_os functions

--- install.py
+++ install.py
@@ -1,12 +1,28 @@
 import platform
 
-if platform.system() == "Linux":
+os = platform.system()
+
+if os == "Linux":
     import linux
 
     distro_name = linux.find_distro_name()
     linux.copy_or_make_vim_vimrc()
     linux.install_dependencys(distro_name)
-    linux.install_needed_font(distro_name)
+    linux.install_needed_font()
     linux.install_vundle()
     linux.copy_vimrc()
     linux.copy_vimspector()
+
+elif os == "Darwin":
+    import mac_os
+
+    mac_os.copy_or_make_vim_vimrc()
+    mac_os.install_homebrew()
+    mac_os.install_dependencys()
+    mac_os.install_needed_font()
+    mac_os.install_vundle()
+    mac_os.copy_vimrc()
+    mac_os.copy_vimspector()
+
+elif os == "Windows":
+    pass
