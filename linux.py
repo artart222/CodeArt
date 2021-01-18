5e1869138dbac6ae48cb22ad6fc90ba612a3c43d
Artin Mobasher <mobasherartin.icm@gmail.com>
2021-01-18 21:55:40 +0330

    some print function added for printing log in terminal and add some lines that install dependencys on rhel and opensuse based ditros

--- linux.py
+++ linux.py
@@ -16,22 +16,29 @@ def get_sudo_access():
 
 
 def find_distro_name():
+    print("Finding which distribution is your os based on")
     distro_name = distro.like()
     if distro_name == "":
         distro_name = distro.id()
+    print("Your distro is " + distro_name + " like")
+
     return distro_name
 
 
 def copy_or_make_vim_vimrc():
     if ".vimrc" in items_in_home_directory:
         shutil.move(home_directory_address + "/.vimrc", home_directory_address + "/.vimrc-copy.vim")
+        print("Making backup of your .vimrc")
     if ".vim" in items_in_home_directory:
         shutil.move(home_directory_address + "/.vim", home_directory_address + "/.vim-copy/")
+        print("Making backup of your .vim")
 
 
 def install_dependencys(distro_name):
     #listing installed apps
     list_of_apps = os.listdir("/bin")
+
+    print("Installing dependencys")
 
     #installing vim
     if "vim" in list_of_apps:
@@ -39,9 +46,13 @@ def install_dependencys(distro_name):
     else:
         print("vim is not installed.\ninstalling vim")
         if distro_name == "arch":
-            os.system("sudo pacman -S vim --noconfirm")
+            os.system("sudo pacman -Sy vim --noconfirm")
         elif distro_name == "debian":
-            os.system("sudo apt install vim -y")
+            os.system("sudo apt update; sudo apt install vim -y")
+        elif distro_name == "rhel":
+            os.system("sudo dnf update -y; sudo dnf install vim-enhanced -y")
+        elif distro_name == "opensuse":
+            os.system("sudo zypper ref; sudo zypper -n vim")
 
     #installing curl
     if "curl" in list_of_apps:
@@ -49,9 +60,13 @@ def install_dependencys(distro_name):
     else:
         print("curl is not installed.\ninstalling curl")
         if distro_name == "arch":
-            os.system("sudo pacman -S curl --noconfirm")
+            os.system("sudo pacman -Sy curl --noconfirm")
         elif distro_name == "debian":
-            os.system("sudo apt install curl -y")
+            os.system("sudo apt update; sudo apt install curl -y")
+        elif distro_name == "rhel":
+            os.system("sudo dnf update -y; sudo dnf install curl -y")
+        elif distro_name == "opensuse":
+            os.system("sudo zypper ref; sudo zypper -n curl")
 
     #installing git
     if "git" in list_of_apps:
@@ -59,9 +74,13 @@ def install_dependencys(distro_name):
     else:
         print("git is not installed.\ninstalling git")
         if distro_name == "arch":
-            os.system("sudo pacman -S git --noconfirm")
+            os.system("sudo pacman -Sy git --noconfirm")
         elif distro_name == "debian":
-            os.system("sudo apt install git -y")
+            os.system("sudo apt update; sudo apt install git -y")
+        elif distro_name == "rhel":
+            os.system("sudo dnf update -y; sudo dnf install git -y")
+        elif distro_name == "opensuse":
+            os.system("sudo zypper ref; sudo zypper -n git")
 
     #installing unzip
     if "unzip" in list_of_apps:
@@ -69,9 +88,14 @@ def install_dependencys(distro_name):
     else:
         print("unzip is not installed.\ninstalling unzip")
         if distro_name == "arch":
-            os.system("sudo pacman -S unzip --noconfirm")
+            os.system("sudo pacman -Sy unzip --noconfirm")
         elif distro_name == "debian":
-            os.system("sudo apt install unzip -y")
+            os.system("sudo apt update; sudo apt install unzip -y")
+        elif distro_name == "rhel":
+            os.system("sudo dnf update -y; sudo dnf install unzip -y")
+        elif distro_name == "opensuse":
+            os.system("sudo zypper ref; sudo zypper -n unzip")
+
 
     #installing ctags
     if "ctags" in list_of_apps:
@@ -79,9 +103,13 @@ def install_dependencys(distro_name):
     else:
         print("ctags is not installed.\ninstalling ctags")
         if distro_name == "arch":
-            os.system("sudo pacman -S ctags --noconfirm")
+            os.system("sudo pacman -Sy ctags --noconfirm")
         elif distro_name == "debian":
-            os.system("sudo apt install ctags -y")
+            os.system("sudo apt update; sudo apt install ctags -y")
+        elif distro_name == "rhel":
+            os.system("sudo dnf update -y; sudo dnf install ctags -y")
+        elif distro_name == "opensuse":
+            os.system("sudo zypper ref; sudo zypper -n ctags")
 
 
 def install_needed_font():
