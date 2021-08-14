1f54f0257576a542a89736d5311869250353297f
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-08-15 00:44:22 +0430

    remove some plugins and add some plugins

--- configs/lua/plugins.lua
+++ configs/lua/plugins.lua
@@ -1,190 +1,151 @@
-vim.cmd('filetype on')
-
 return require('packer').startup(function()
   use { 'wbthomason/packer.nvim' }
 
-  -- Color schemes
-  use { 'marko-cerovac/material.nvim' }
-  require('plugins/material')
-
-  use { 'Shadorain/shadotheme' }
+  -- Color schemes.
   use { 'folke/tokyonight.nvim' }
   use { 'bluz71/vim-nightfly-guicolors' }
   use { 'bluz71/vim-moonfly-colors' }
-  use { 'sainnhe/sonokai' }
-  use { 'mhartington/oceanic-next' }
-  use { 'fenetikm/falcon' }
   use { 'shaunsingh/nord.nvim' }
-  use { 'shaunsingh/moonlight.nvim' }
   use { 'navarasu/onedark.nvim' }
-  use { 'NTBBloodbath/doom-one.nvim' }
-  use { 'dracula/vim' }
-  use { 'nxvu699134/vn-night.nvim' }
-  use { 'projekt0n/github-nvim-theme' }
 
-  -- Color sheme switcher and it dependecy
-  use { 'xolox/vim-colorscheme-switcher' }
-  use { 'xolox/vim-misc'}
+  -- TrueZen.nvim is a Neovim plugin that aims to provide a cleaner and less cluttered interface
+  -- when toggled in either of it's three different modes (Ataraxis, Minimalist and Focus).
+  use { 'Pocco81/TrueZen.nvim' }
 
-  -- Plugin that dims inactive windows
-  use { 'artart222/Shade.nvim' }
-  require('plugins/shade')
+  -- This plugin adds indentation guides to all lines (including empty lines).
+  use { 'lukas-reineke/indent-blankline.nvim' }
 
-  -- Opens the current buffer in a new full-screen floating window without
-  -- messing up with existing window layouts
-  use { 'folke/zen-mode.nvim',
-    config = function()
-      require("zen-mode").setup()
-    end
-  }
-
-  use { 'lukas-reineke/indent-blankline.nvim'}
-  vim.cmd('let g:indent_blankline_filetype_exclude = ["dashboard"]')
-
-  -- This plugin causes all trailing whitespace characters to be highlighted
+  -- This plugin causes all trailing whitespace characters to be highlighted.
   use { 'ntpeters/vim-better-whitespace' }
 
-  -- Maximizes and restores the current window in nvim
-  use { 'szw/vim-maximizer' }
-
-  -- Icons
+  -- Icons.
   use { 'kyazdani42/nvim-web-devicons' }
 
-  -- File explorer tree
+  -- File explorer tree.
   use { 'kyazdani42/nvim-tree.lua' }
-  require('plugins/nvim-tree')
 
-  -- Bufferline
+  -- Bufferline.
   use { 'akinsho/nvim-bufferline.lua' }
-  require('plugins/bufferline')
 
-  -- Statusline
+  -- Statusline.
   use { 'hoob3rt/lualine.nvim' }
-  require('plugins/lualine')
 
-  -- TreeSitter
+  -- TreeSitter.
   use { 'nvim-treesitter/nvim-treesitter' }
-  require('plugins/treesitter')
 
-  -- Colorizer (for highlighting color codes)
+  -- Colorizer (for highlighting color codes).
   use { 'norcalli/nvim-colorizer.lua' }
-  require('plugins/colorize')
 
-  -- VsCode like pictograms
-  use { 'onsails/lspkind-nvim' }
-  require('plugins/lspkind')
-
-  -- Startup screen
+  -- Startup screen.
   use { 'glepnir/dashboard-nvim' }
-  require('plugins/dashboard')
 
-  -- Fuzzy finder and it requirments
+  -- Fuzzy finder and it requirments.
   use { 'nvim-telescope/telescope.nvim' }
+  use { 'nvim-telescope/telescope-media-files.nvim' }
   use { 'nvim-lua/popup.nvim' }
   use { 'nvim-lua/plenary.nvim' }
 
-  -- LSP and completion
+  -- LSP and completion.
   use { 'neovim/nvim-lspconfig' }
   use { 'hrsh7th/nvim-compe' }
-  require('plugins/compe')
 
-  -- LSP signature
+  -- LSP signature.
   use { "ray-x/lsp_signature.nvim" }
-  require("lsp_signature").setup()
 
-  -- View and search LSP symbols, tags in Neovim
+  -- VsCode like pictograms for lsp.
+  use { 'onsails/lspkind-nvim' }
+
+  -- View and search LSP symbols, tags in Neovim.
   use { 'liuchengxu/vista.vim' }
 
-  -- Terminal
+  -- Terminal.
   use { 'akinsho/nvim-toggleterm.lua' }
-  require('plugins/toggleterm')
 
-  -- Floating terminal
+  -- Floating terminal.
   use { 'voldikss/vim-floaterm' }
 
-  -- Git support for nvim
+  -- Git support for nvim.
   use { 'tpope/vim-fugitive' }
 
-  -- Git signs
-  use { 'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
+  -- Git signs.
+  use { 'lewis6991/gitsigns.nvim' }
 
-  -- Lazygit support for nvim
-  use { 'kdheepak/lazygit.nvim' }
-
-  -- Git commit browser
+  -- Git commit browser.
   use { 'junegunn/gv.vim' }
 
-  -- Edit and review GitHub issues and pull requests from nvim
-  use {'pwntester/octo.nvim'}
-  require"octo".setup()
-
-  -- Auto closes
+  -- Auto closes.
   use { 'windwp/nvim-ts-autotag' } -- This is for html and it can autorename too!
-  require('nvim-autopairs').setup()
   use { 'windwp/nvim-autopairs' }
 
-  -- Scrollbar
+  -- Scrollbar.
   use { 'dstein64/nvim-scrollview' }
-  vim.cmd('highlight ScrollView ctermbg=160 guibg=LightCyan') -- Set scrollbar color to LightCyan
 
-  -- Smooth scroll
+  -- Smooth scroll.
   use { 'karb94/neoscroll.nvim' }
-  require('neoscroll').setup()
 
-  -- Markdow preview
+  -- Markdow preview.
   use { 'iamcco/markdown-preview.nvim' }
-  require('plugins/markdown')
 
-  -- Ranger
+  -- Ranger.
   use { 'kevinhwang91/rnvimr' }
-  require('plugins/ranger')
 
   -- todo-comments is a lua plugin for Neovim to highlight and search for
-  -- todo comments like TODO, HACK, BUG in code base
+  -- todo comments like TODO, HACK, BUG in code base.
   use { 'folke/todo-comments.nvim' }
-  require('plugins/todo-comments')
 
   -- WhichKey is a lua plugin that displays a popup with possible
-  -- key bindings of the command you started typing
-  use { 'folke/which-key.nvim',
-    config = function()
-      require("which-key").setup()
-    end
-  }
+  -- key bindings of the command you started typing.
+  use { 'folke/which-key.nvim' }
 
   -- A plugin for neovim that automatically creates missing directories
-  -- on saving a fileA
-  use { 'jghauser/mkdir.nvim',
-    config = function()
-      require('mkdir')
-    end
-  }
+  -- on saving a file.
+  use { 'jghauser/mkdir.nvim' }
 
   -- Dev setup for init.lua and plugin development with full signature
-  -- help, docs and completion for the nvim lua API
+  -- help, docs and completion for the nvim lua API.
   use { 'folke/lua-dev.nvim' }
 
   -- Neovim plugin to comment text in and out.
-  -- Supports commenting out the current line, a visual selection and a motion
+  -- Supports commenting out the current line, a visual selection and a motion.
   use { 'b3nj5m1n/kommentary' }
 
-  -- JABS is a minimal buffer switcher window for Neovim.
-  use { 'matbme/JABS.nvim' }
-
-  -- Easily adds brief author info and license headers
+  -- Easily adds brief author info and license headers.
   use { 'alpertuna/vim-header' }
-  require('plugins/header')
 
+  -- Add some features to Neovim for building projects for example add :Make.
+  -- Make is like :make command but it make separate tab for builing projects.
   use { 'tpope/vim-dispatch' }
+
   -- The plugin is a capable Vim debugger for multiple languages.
   -- It's mostly tested for C++, Python and TCL, but in theory supports
   -- any language that Visual Studio Code supports.
   use { 'puremourning/vimspector' }
 
-  -- match-up is a plugin that lets you highlight, navigate, and operate on sets of matching text
+  -- match-up is a plugin that lets you highlight, navigate, and operate on sets of matching text.
   use { 'andymass/vim-matchup' }
+
+  -- Import settings of plugins or start plugins.
+  require('plugins/dashboard')
+  require('plugins/indent-blankline')
+  require('plugins/nvim-tree')
+  require('plugins/bufferline')
+  require('plugins/lualine')
+  require('plugins/true-zen')
+  require('plugins/treesitter')
+  require('plugins/colorize')
+  require('plugins/lspkind')
+  require('plugins/compe')
+  require('lsp_signature').setup()
+  require('plugins/toggleterm')
+  require('gitsigns').setup()
+  require('nvim-autopairs').setup()
+  require('neoscroll').setup()
+  vim.cmd('highlight ScrollView ctermbg=160 guibg=LightCyan') -- Set scrollbar color to LightCyan
+  require('plugins/markdown')
+  require('plugins/ranger')
+  require('plugins/todo-comments')
+  require("which-key").setup()
+  require('mkdir')
+  require('plugins/header')
+  require('plugins/telescope')
 end )
