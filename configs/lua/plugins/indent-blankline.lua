a804a59a05c8e4b1fc8ae36b9f2d4aec41a52bc3
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-08-15 00:47:45 +0430

    Add settings for true-zen, telescope and indent-blankline plugins.

---
+++ configs/lua/plugins/indent-blankline.lua
@@ -0,0 +1,7 @@
+  vim.g.indent_blankline_char = "▏"
+
+  vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard", "packer"}
+  vim.g.indent_blankline_buftype_exclude = {"terminal"}
+
+  vim.g.indent_blankline_show_trailing_blankline_indent = false
+  vim.g.indent_blankline_show_first_indent_level = true
---
+++ configs/lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+require'telescope'.setup {
+  extensions = {
+    media_files = {
+      -- filetypes whitelist
+      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+      filetypes = {"png", "webp", "jpg", "jpeg"},
+      find_cmd = "rg" -- find command (defaults to `fd`)
+    }
+  },
+}
