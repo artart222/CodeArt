aa32dc3da3dcd2b6d00749d7ac7f7b0d2335d1a4
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-08-15 17:33:06 +0430

    Add two useless comment to thid file too!

--- configs/lua/plugins/indent-blankline.lua
+++ configs/lua/plugins/indent-blankline.lua
@@ -1,5 +1,7 @@
+  -- indent-blankline character.
   vim.g.indent_blankline_char = "▏"
 
+  -- Disable indent-blankline on these pages.
   vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard", "packer"}
   vim.g.indent_blankline_buftype_exclude = {"terminal"}
 
