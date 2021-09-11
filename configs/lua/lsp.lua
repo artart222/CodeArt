fa71edebf16b90989eaa492bf9bf02207d057140
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-09-11 07:56:01 +0430

    add lsp install configs and move from pylsp and ccls to pyright and clangd

--- configs/lua/lsp.lua
+++ configs/lua/lsp.lua
@@ -7,10 +7,10 @@ require'lspconfig'.html.setup { capabilities = capabilities }
 require'lspconfig'.cssls.setup { capabilities = capabilities }
 
 -- C/CPP/Objective-C
-require'lspconfig'.ccls.setup {}
+require'lspconfig'.clangd.setup{}
 
 -- Python
-require'lspconfig'.pylsp.setup {}
+require'lspconfig'.pyright.setup{}
 
 -- Javascript
 require'lspconfig'.denols.setup {}
@@ -29,3 +29,20 @@ require'lspconfig'.rust_analyzer.setup {}
 
 -- C# (omnisharp)
 require'lspconfig'.omnisharp.setup {}
+
+
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{}
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
