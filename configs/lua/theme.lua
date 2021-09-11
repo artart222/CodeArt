ac17cc0094924c91306b539cd47cb9206a44c9a0
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-09-11 07:44:49 +0430

    default theme changed to onedark

--- configs/lua/theme.lua
+++ configs/lua/theme.lua
@@ -3,4 +3,4 @@ vim.opt.fillchars = {eob = " "}
 
 vim.g.tokyonight_style = 'night' -- styles: storm, night and day.
 vim.g.onedark_style = 'deep'     -- styles: dark, darker, cool, deep, warm and warmer.
-vim.cmd('colorscheme tokyonight')
+vim.cmd('colorscheme onedark')
