e04395a08f04512293c0856dbd500e71a0a4373e
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-09-11 07:45:24 +0430

    removed auto closing

--- configs/lua/plugins/markdown.lua
+++ configs/lua/plugins/markdown.lua
@@ -2,7 +2,7 @@ vim.cmd
 [[
   let g:mkdp_auto_start = 0
   let g:mkdp_auto_open = 0
-  let g:mkdp_auto_close = 1
+  let g:mkdp_auto_close = 0
   let g:mkdp_refresh_slow = 0
   let g:mkdp_command_for_global = 0
   let g:mkdp_open_to_the_world = 0
