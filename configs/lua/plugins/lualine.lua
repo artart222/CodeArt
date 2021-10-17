5243b837c30728dc886879e3151825be2449f3ba
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-10-17 18:17:47 +0330

    Disable lueline for vista

--- configs/lua/plugins/lualine.lua
+++ configs/lua/plugins/lualine.lua
@@ -1,4 +1,4 @@
 require('lualine').setup {
-  options = {theme = 'auto', disabled_filetypes = {'toggleterm', 'NvimTree'}},
+  options = {theme = 'auto', disabled_filetypes = {'toggleterm', 'NvimTree', 'vista'}},
   extensions = {'fugitive'}
 }
