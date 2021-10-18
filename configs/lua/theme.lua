a7061bf517e56023bf04550a301a2d629799f614
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-10-18 19:52:09 +0330

    Add icons for diagnostics

--- configs/lua/theme.lua
+++ configs/lua/theme.lua
@@ -1,6 +1,15 @@
 -- Hide ~ from end of lines.
 vim.opt.fillchars = {eob = " "}
 
+-- Add icons for lsp diagnostics sings
+vim.cmd
+[[
+  sign define LspDiagnosticsSignError text= texthl=LspDiagnosticsSignError linehl= numhl=
+  sign define LspDiagnosticsSignWarning text= texthl=LspDiagnosticsSignWarning linehl= numhl=
+  sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=
+  sign define LspDiagnosticsSignHint text= texthl=LspDiagnosticsSignHint linehl= numhl=
+]]
+
 vim.g.tokyonight_style = 'night' -- styles: storm, night and day.
 vim.g.onedark_style = 'deep'     -- styles: dark, darker, cool, deep, warm and warmer.
 vim.cmd('colorscheme onedark')
