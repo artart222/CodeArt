46eb9c294e7eb4d3d10910d583e2f6909c2e0590
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-10-20 18:53:29 +0330

    Remove unimportant lines

--- configs/lua/lsp.lua
+++ configs/lua/lsp.lua
@@ -1,41 +1,8 @@
--- Enable (broadcasting) snippet capability for completion
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
--- Html/css
-require'lspconfig'.html.setup { capabilities = capabilities }
-require'lspconfig'.cssls.setup { capabilities = capabilities }
-
--- C/CPP/Objective-C
-require'lspconfig'.clangd.setup{}
-
--- Python
-require'lspconfig'.pyright.setup{}
-
--- Javascript
-require'lspconfig'.denols.setup {}
-
--- Lua
-local luadev = require("lua-dev").setup {
-  lspconfig = {
-    cmd = {"lua-language-server"}
-  },
-}
-local lspconfig = require('lspconfig')
-lspconfig.sumneko_lua.setup(luadev)
-
--- Rust
-require'lspconfig'.rust_analyzer.setup {}
-
--- C# (omnisharp)
-require'lspconfig'.omnisharp.setup {}
-
-
 local function setup_servers()
   require'lspinstall'.setup()
   local servers = require'lspinstall'.installed_servers()
   for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
+    require'lspconfig'[server].setup {}
   end
 end
 
