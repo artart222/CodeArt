1c1981abc339f95b4dfdb145a55e3b2a919c55c5
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-10-20 18:52:32 +0330

    Convert some vimscripts to lua

--- configs/lua/settings.lua
+++ configs/lua/settings.lua
@@ -1,9 +1,5 @@
--- Defining aliases.
-local cmd = vim.cmd
+-- Defining alias for vim.opt.
 local opt = vim.opt
-
--- Enabling syntax highlighting.
-cmd('syntax on')
 
 -- Number settings.
 opt.number = true
@@ -25,24 +21,19 @@ opt.cursorline = true
 -- Setting colorcolumn. This is set becuse of
 -- this (https://github.com/lukas-reineke/indent-blankline.nvim/issues/59)
 -- indent-blankline bug.
-cmd('set colorcolumn=99999')
+opt.colorcolumn = '9999'
 
 -- With set hidden you’re telling Neovim that you can
 -- have unsaved worked that’s not displayed on your screen.
 opt.hidden = true
 
 -- Set indentation stuf.
-cmd('filetype plugin indent on')
-opt.smarttab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
 opt.smartindent = true
-
--- Set indentation for some languages manually.
-vim.cmd('autocmd Filetype javascript,python,cpp,c setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4')
-vim.cmd('autocmd Filetype lua,html,css setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2')
-
--- Go to previous/next line with h,l,left arrow and right arrow
--- when cursor reaches end/beginning of line.
--- opt.whichwrap:append("<>hl")
+opt.smartcase = true
+opt.expandtab = true
+opt.smarttab = true
 
 -- Set searching stuf.
 opt.hlsearch = true
@@ -72,7 +63,7 @@ opt.timeoutlen = 1000
 opt.completeopt = "menuone,noselect"
 
 -- Set line number for help files.
-cmd
+vim.cmd
 [[
   augroup help_config
     autocmd!
