0baaba2e66cc9c3c70507e62c996fda4efd0878b
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-10-22 02:03:11 +0330

    Fix some indentation

--- configs/lua/plugins/indent-blankline.lua
+++ configs/lua/plugins/indent-blankline.lua
@@ -1,9 +1,9 @@
-  -- indent-blankline character.
-  vim.g.indent_blankline_char = "▏"
+-- indent-blankline character.
+vim.g.indent_blankline_char = "▏"
 
-  -- Disable indent-blankline on these pages.
-  vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard", "packer"}
-  vim.g.indent_blankline_buftype_exclude = {"terminal"}
+-- Disable indent-blankline on these pages.
+vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard", "packer"}
+vim.g.indent_blankline_buftype_exclude = {"terminal"}
 
-  vim.g.indent_blankline_show_trailing_blankline_indent = false
-  vim.g.indent_blankline_show_first_indent_level = true
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_first_indent_level = true
