cb4fa38c1fd1fdc854294bf2384795c626ffceaa
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-11-01 21:29:31 +0330

    Add shebang to top of file

--- configs/CodeArtUpdate.sh
+++ configs/CodeArtUpdate.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 mv ~/.config/nvim/lua/user_settings.lua ~/.config/
 svn export --force https://github.com/artart222/CodeArt.git/trunk/configs/ ~/.config/nvim
 mv --force ~/.config/user_settings.lua ~/.config/nvim/lua/user_settings.lua
