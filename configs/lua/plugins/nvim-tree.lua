9a8165a5a89e809327bfb3a7d392a48270e19b20
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-11-04 01:51:45 +0330

    Change some settings of nvim-tree and use style of other files in it

--- configs/lua/plugins/nvim-tree.lua
+++ configs/lua/plugins/nvim-tree.lua
@@ -6,9 +6,8 @@ end
 -- Set alias for vim.g.
 local g = vim.g
 
-g.nvim_tree_ignore = {".git", "node_modules", ".cache", "__pycache__"} -- Ignore these types in listing.
-g.nvim_tree_gitignore = 1 -- Enable gitignore.
-g.nvim_tree_auto_ignore_ft = {"dashboard"} -- Don't open tree on specific fiypes.
+g.nvim_tree_ignore = { '.git', 'node_modules', '.cache', '__pycache__'} -- Ignore these types in listing.
+g.nvim_tree_auto_ignore_ft = { 'dashboard' } -- Don't open tree on specific fiypes.
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened.
 g.nvim_tree_indent_markers = 1 -- This option shows indent markers when folders are open.
 g.nvim_tree_hide_dotfiles = 1 -- This option hides files and folders starting with a dot `.`.
@@ -16,7 +15,7 @@ g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be 
 g.nvim_tree_highlight_opened_files = 0 -- Will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_add_trailing = 0 -- Append a trailing slash to folder names. ]]
 
-nvimtree.setup ({
+nvimtree.setup {
   auto_close = false,
   open_on_tab = false,
   update_cwd = true,
@@ -36,8 +35,8 @@ nvimtree.setup ({
   view = {
     width = "20%",
     side = "left",
-    }
-})
+  }
+}
 
 -- Hide statusline in nvim-tree buffer/tabs.
-vim.cmd [[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif]]
+vim.cmd('au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname("%") == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif')
