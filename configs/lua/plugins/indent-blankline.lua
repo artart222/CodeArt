e18dd02eafc5751ec55729a790fd0eea5495829a
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-11-09 19:10:48 +0330

    excludes updated

--- configs/lua/plugins/indent-blankline.lua
+++ configs/lua/plugins/indent-blankline.lua
@@ -2,8 +2,16 @@
 vim.g.indent_blankline_char = "▏"
 
 -- Disable indent-blankline on these pages.
-vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard", "packer"}
-vim.g.indent_blankline_buftype_exclude = { "terminal", 'lsp-installer', 'lspinfo' }
+vim.g.indent_blankline_filetype_exclude = {
+  "help",
+  "terminal",
+  "dashboard",
+  "packer",
+  "lsp-installer",
+  "lspinfo",
+  "vista_kind"
+}
+vim.g.indent_blankline_buftype_exclude = { "terminal" }
 
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
