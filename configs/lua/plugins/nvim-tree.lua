28622cb7acfa2a95778ed7d4241b6c58dea1c31b
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-11-09 19:19:50 +0330

    Change mapping for dotfiles and resizing and add 2 filetype and a buftype for window picker exclude

--- configs/lua/plugins/nvim-tree.lua
+++ configs/lua/plugins/nvim-tree.lua
@@ -2,6 +2,7 @@ local present, nvimtree = pcall(require, "nvim-tree")
 if not present then
     return
 end
+local tree_cb = require"nvim-tree.config".nvim_tree_callback
 
 -- Set alias for vim.g.
 local g = vim.g
@@ -14,6 +15,10 @@ g.nvim_tree_hide_dotfiles = 1 -- This option hides files and folders starting wi
 g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 0 -- Will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_add_trailing = 0 -- Append a trailing slash to folder names. ]]
+vim.g.nvim_tree_window_picker_exclude = {
+    filetype = {'packer', 'vista_kind'},
+    buftype = {'terminal'}
+}
 
 nvimtree.setup {
   auto_close = false,
@@ -35,8 +40,13 @@ nvimtree.setup {
   view = {
     width = "20%",
     side = "left",
+    mappings = {
+      list = {
+        {
+          key = "<S-h>", cb = ":call ResizeLeft(3)<CR>",
+          key = "<C-h>", cb = tree_cb("toggle_dotfiles"),
+        },
+      }
+    }
   }
 }
-
--- Hide statusline in nvim-tree buffer/tabs.
-vim.cmd("au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname(\"%\") == \"NvimTree\" | set laststatus=0 | else | set laststatus=2 | endif")
