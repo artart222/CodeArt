fcb41efde795f565848526b9a4384189a2b103f4
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-11-16 04:19:16 +0330

    Add find_directories to maps.lua and dashboard.lua

--- configs/lua/plugins/dashboard.lua
+++ configs/lua/plugins/dashboard.lua
@@ -26,11 +26,12 @@ g.dashboard_custom_header = {
 
 g.dashboard_custom_section = {
     a = { description = { "  Find File                 SPC f f" }, command = "Telescope find_files" },
-    b = { description = { "  Recents                   SPC f o" }, command = "Telescope oldfiles" },
-    c = { description = { "  Find Word                 SPC f w" }, command = "Telescope live_grep" },
-    d = { description = { "洛 New File                  SPC f n" }, command = "DashboardNewFile" },
-    e = { description = { "  Bookmarks                 SPC b m" }, command = "Telescope marks" },
-    f = { description = { "  Load Last Session         SPC s l" }, command = "SessionLoad" }
+    b = { description = { "  Find directory            SPC f d" }, command = "Telescope find_directories" },
+    c = { description = { "  Recents                   SPC f o" }, command = "Telescope oldfiles" },
+    d = { description = { "  Find Word                 SPC f w" }, command = "Telescope live_grep" },
+    e = { description = { "洛 New File                  SPC f n" }, command = "DashboardNewFile" },
+    f = { description = { "  Bookmarks                 SPC b m" }, command = "Telescope marks" },
+    g = { description = { "  Load Last Session         SPC s l" }, command = "SessionLoad" }
 }
 
 g.dashboard_custom_footer = {
