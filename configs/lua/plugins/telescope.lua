a38fc6bebfc1c8eb5717fce7eec3032e741bae31
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-12-22 22:34:26 +0330

    The telescope does not preview binaries now

--- configs/lua/plugins/telescope.lua
+++ configs/lua/plugins/telescope.lua
@@ -3,9 +3,33 @@ if not present then
     return
 end
 
+local previewers = require("telescope.previewers")
+local Job = require("plenary.job")
+local new_maker = function(filepath, bufnr, opts)
+  filepath = vim.fn.expand(filepath)
+  Job:new({
+    command = "file",
+    args = { "--mime-type", "-b", filepath },
+    on_exit = function(j)
+      local mime_type = vim.split(j:result()[1], "/")[1]
+      if mime_type == "text" then
+        previewers.buffer_previewer_maker(filepath, bufnr, opts)
+      else
+        -- maybe we want to write something to the buffer here
+        vim.schedule(function()
+          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+        end)
+      end
+    end
+  }):sync()
+end
+
 local os = vim.loop.os_uname().sysname
 if os == "Linux" and vim.fn.has("wsl") == 0 then
   telescope.setup {
+    defaults = {
+      buffer_previewer_maker = new_maker,
+    },
     extensions = {
       media_files = {
         filetypes = { "png", "webp", "jpg", "jpeg" },
