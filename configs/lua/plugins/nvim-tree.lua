422769843f146339711be0d2d84a50d13badc37a
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-12-22 22:22:34 +0330

    Changed some nvim-tree settings

--- configs/lua/plugins/nvim-tree.lua
+++ configs/lua/plugins/nvim-tree.lua
@@ -7,11 +7,9 @@ local tree_cb = require"nvim-tree.config".nvim_tree_callback
 -- Set alias for vim.g.
 local g = vim.g
 
-g.nvim_tree_ignore = { ".git", "node_modules", ".cache", "__pycache__"} -- Ignore these types in listing.
 g.nvim_tree_auto_ignore_ft = { "dashboard" } -- Don't open tree on specific fiypes.
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened.
 g.nvim_tree_indent_markers = 1 -- This option shows indent markers when folders are open.
-g.nvim_tree_hide_dotfiles = 1 -- This option hides files and folders starting with a dot `.`.
 g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 0 -- Will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_add_trailing = 0 -- Append a trailing slash to folder names. ]]
@@ -42,9 +40,18 @@ nvimtree.setup {
     ignore = false,
     timeout = 500,
   },
+  filters = {
+    custom = {
+      ".git",
+      "node_modules",
+      ".cache",
+      "__pycache__"
+    }
+  },
   view = {
     width = "15%",
     side = "left",
+    auto_resize = true,
     mappings = {
       list = {
        {key = "<S-h>", cb = ":call ResizeLeft(3)<CR>"},
