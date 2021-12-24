4ad2b435439089e90c0f6bba3fd403392f81c067
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2021-12-24 17:38:45 +0330

    Changed on of icons

--- configs/lua/plugins/dashboard.lua
+++ configs/lua/plugins/dashboard.lua
@@ -29,7 +29,7 @@ g.dashboard_custom_section = {
     b = { description = { "  Find directory            SPC f d" }, command = "Telescope find_directories" },
     c = { description = { "  Recents                   SPC f o" }, command = "Telescope oldfiles" },
     d = { description = { "  Find Word                 SPC f w" }, command = "Telescope live_grep" },
-    e = { description = { "洛 New File                  SPC f n" }, command = "DashboardNewFile" },
+    e = { description = { "  New File                  SPC f n" }, command = "DashboardNewFile" },
     f = { description = { "  Bookmarks                 SPC b m" }, command = "Telescope marks" },
     g = { description = { "  Load Last Session         SPC s l" }, command = "SessionLoad" }
 }
