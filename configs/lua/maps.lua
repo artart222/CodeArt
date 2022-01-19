1471927c6fcfadbc7baa8625f0f5b27780e3b817
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-01-20 01:05:07 +0330

    Improved CodeArt startup time changed some keymaps and added theme to which-key comment plugin has changed

--- configs/lua/maps.lua
+++ configs/lua/maps.lua
@@ -1,3 +1,5 @@
+local wk = require("which-key")
+
 -- Function for make mapping easier.
 local function map(mode, lhs, rhs, opts)
   local options = {noremap = true}
@@ -8,10 +10,6 @@ end
 
 -- Map leader key to space.
 vim.g.mapleader = " "
-
-
--- Set cl for clearing highlights after searching word in file.
-map("n", "cl", ":noh<CR>")
 
 
 -- Split navigations.
@@ -33,92 +31,10 @@ map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
 map("n", "<Tab>", ":BufferLineCycleNext<CR>")
 
 
--- Buffer closing.
-map("n", "<leader>bc", ":BufferLinePickClose<CR>")
-
-
--- Buffer moving.
-map("n", "<leader>bl", ":BufferLineMoveNext<CR>")
-map("n", "<leader>bh", ":BufferLineMovePrev<CR>")
-
-
--- Don't copy the replaced text after pasting.
-map("v", "p", "\"_dP")
-
-
--- Jk for going to normal mode while you are in insert mode.
-map("i", "jk", "<ESC>")
-
--- With this you can use > < multiple time for changing indent when you visual selected text.
-map("v", "<", "<gv")
-map("v", ">", ">gv")
-
--- Packer
-map("n", "<leader>pi", ":PackerInstall<CR>")
-map("n", "<leader>pu", ":PackerUpdate<CR>")
-map("n", "<leader>pr", ":PackerClean<CR>")
-map("n", "<leader>ps", ":PackerSync<CR>")
-map("n", "<leader>pc", ":PackerCompile<CR>")
-
--- NvimTree toggle
-map("n", "<leader>nt", ":NvimTreeToggle<CR>")
-
-
--- Telescope
-map("n", "<Leader>fw", ":Telescope live_grep<CR>")
-map("n", "<Leader>gt", ":Telescope git_status<CR>")
-map("n", "<Leader>cm", ":Telescope git_commits<CR>")
-map("n", "<Leader>ff", ":Telescope find_files<CR>")
-map("n", "<Leader>fd", ":Telescope find_directories<CR>")
-map("n", "<Leader>fp", ":Telescope media_files<CR>")
-map("n", "<Leader>fb", ":Telescope buffers<CR>")
-map("n", "<Leader>fh", ":Telescope help_tags<CR>")
-map("n", "<Leader>fo", ":Telescope oldfiles<CR>")
-map("n", "<Leader>th", ":Telescope colorscheme<CR>")
-
-
--- Dashboard
+-- Save and load session/Dashboard.
 map("n", "<Leader>db", ":Dashboard<CR>")
-map("n", "<Leader>fn", ":DashboardNewFile<CR>")
-map("n", "<Leader>bm", ":DashboardJumpMarks<CR>")
 map("n", "<C-s>l", ":SessionLoad<CR>")
 map("n", "<C-s>s", ":SessionSave<CR>")
-
-
--- Lsp
-local lsp_opts = { noremap=true, silent=true }
-map("n", "gD", ":lua vim.lsp.buf.declaration()<CR>", lsp_opts)
-map("n", "gd", ":lua vim.lsp.buf.definition()<CR>", lsp_opts)
-map("n", "<leader>k", ":lua vim.lsp.buf.hover()<CR>", lsp_opts)
-map("n", "gi", ":lua vim.lsp.buf.implementation()<CR>", lsp_opts)
-map("n", "<C-k>", ":lua vim.lsp.buf.signature_help()<CR>", lsp_opts)
-map("n", "<leader>wa", ":lua vim.lsp.buf.add_workspace_folder()<CR>", lsp_opts)
-map("n", "<leader>wr", ":lua vim.lsp.buf.remove_workspace_folder()<CR>", lsp_opts)
-map("n", "<leader>wl", ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", lsp_opts)
-map("n", "<leader>D", ":lua vim.lsp.buf.type_definition()<CR>", lsp_opts)
-map("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>", lsp_opts)
-map("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>", lsp_opts)
-map("n", "gr", ":lua vim.lsp.buf.references()<CR>", lsp_opts)
-map("n", "<leader>e", ":lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", lsp_opts)
-map("n", "[d", ":lua vim.lsp.diagnostic.goto_prev()<CR>", lsp_opts)
-map("n", "]d", ":lua vim.lsp.diagnostic.goto_next()<CR>", lsp_opts)
-map("n", "<leader>q", ":lua vim.lsp.diagnostic.set_loclist()<CR>", lsp_opts)
-
-
--- Dap
-map("n", "<F5>", ":lua require(\"dap\").continue()<CR>")
-map("n", "<leader>te", ":lua require(\"dap\").terminate()<CR>")
-map("n", "<leader>br", ":lua require(\"dap\").toggle_breakpoint()<CR>")
-map("n", "<leader>Br", ":lua require(\"dap\").set_breakpoint(vim.fn.input(\"Breakpoint condition: \"))<CR>")
-map("n", "<leader>lp", ":lua require(\"dap\").set_breakpoint(nil, nil, vim.fn.input(\"Log point message: \"))<CR>")
-map("n", "<F10>", ":lua require(\"dap\").step_over()<CR>")
-map("n", "<F11>", ":lua require(\"dap\").step_into()<CR>")
-map("n", "<F12>", ":lua require(\"dap\").step_out()<CR>")
-map("n", "<leader>sb", ":lua require(\"dap\").step_back()<CR>")
-map("n", "<leader>rc", ":lua require(\"dap\").run_to_cursor()<CR>")
-map("n", "<leader>ro", ":lua require(\"dap\").repl.open()<CR>")
-map("n", "<leader>dt", ":lua require(\"dapui\").toggle()<CR>")
-map("n", "<leader>dl", ":lua require(\"dap\").run_last()<CR>")
 
 
 -- ToggleTerm
@@ -137,18 +53,167 @@ end
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 
--- Remove unnecessary white spaces.
-map("n", "<leader>cw", ":StripWhitespace<CR>")
-
-
--- TrueZen focus mode.
-map("n", "<leader>fs", ":TZFocus<CR>")
-
-
 -- comment
-map("n", "ct", ":CommentToggle<CR>")
-map("v", "ct", ":'<,'>CommentToggle<CR>")
+map("n", "ct", ":lua require(\"Comment.api\").toggle_linewise_op()<CR>")
+map("v", "ct", ":lua require(\"Comment.api\").toggle_blockwise_op(vim.fn.visualmode())<CR>")
+map("x", "ct", ":lua require(\"Comment.api\").toggle_blockwise_op(vim.fn.visualmode())<CR>")
 
 
--- Code formatter.
-map("n", "<leader>fr", ":Neoformat<CR>", lsp_opts)
+-- Don't copy the replaced text after pasting.
+map("v", "p", "\"_dP")
+
+
+-- Jk for going to normal mode while you are in insert mode.
+map("i", "jk", "<ESC>")
+
+
+-- With this you can use > < multiple time for changing indent when you visual selected text.
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+
+-- Buffer
+wk.register({
+  b = {
+    name = "Buffer",
+    c = { ":BufferLinePickClose<CR>", "Close Buffer" },
+    e = { ":noh<CR>", "Erase Search Highlights" },
+    l = { ":BufferLineMoveNext<CR>", "Move Buffer Right" },
+    h = { ":BufferLineMovePrev<CR>", "Move buffer Left" },
+    n = { ":DashboardNewFile<CR>", "New Buffer"},
+    r = { ":Neoformat<CR>", "Format Buffer" },
+    m = { ":TZFocus<CR>", "Maximize Current Buffer" },
+  }
+}, { prefix = "<leader>" })
+
+
+-- Packer
+wk.register({
+  p = {
+    name = "Packer",
+    i = { ":PackerInstall<CR>", "Install CodeArt packages" },
+    u = { ":PackerUpdate<CR>", "Update CodeArt packages" },
+    r = { ":PackerClean<CR>", "Uninstall unnecessary CodeArt packages" },
+    s = { ":PackerSync<CR>", "Sync CodeArt packages" },
+    c = { ":PackerClean<CR>", "Compile CodeArt packages" },
+  }
+}, { prefix = "<leader>" })
+
+
+-- NvimTree
+wk.register({
+  n = {
+    name = "NvimTree",
+    t = { ":NvimTreeToggle<CR>", "Toggle NvimTree" },
+    f = { ":NvimTreeFocus<CR>", "Focus on NvimTree" },
+  }
+}, { prefix = "<leader>" })
+
+
+-- Finding different stuf.
+wk.register({
+  f = {
+    name = "Find",
+    w = { ":Telescope live_grep<CR>", "Word" },
+    f = { ":Telescope find_files<CR>", "File" },
+    o = { ":Telescope oldfiles<CR>", "Old File" },
+    d = { ":Telescope find_directories<CR>", "Directory" },
+    p = { ":Telescope media_files<CR>", "Media File" },
+    b = { ":Telescope buffers<CR>", "Buffer" },
+    h = { ":Telescope help_tags<CR>", "Help File" },
+    B = { ":DashboardJumpMarks<CR>", "Find BookMark" },
+  }
+}, { prefix = "<leader>" })
+
+
+-- Git keybinds.
+wk.register({
+  g = {
+    name = "Git",
+    s = { ":Telescope git_status<CR>", "Status + Git Diff" },
+    c = { ":Telescope git_commits<CR>", "Commit History" },
+    C = { ":Telescope git_bcommits<CR>", "Buffer Commit History" },
+    b = { ":Telescope git_branches<CR>", "Branches history" },
+    k = { ":Gitsigns prev_hunk<CR>", "Next Hunk" },
+    j = { ":Gitsigns next_hunk<CR>", "Prev Hunk" },
+    p = { ":Gitsigns preview_hunk<CR>", "Preview Hunk" },
+    r = { ":Gitsigns reset_hunk<CR>", "Reset Hunk" },
+    R = { ":Gitsigns reset_buffer<CR>", "Reset Buffer" },
+    d = { ":Gitsigns diffthis<CR>", "Git Diff" },
+    l = { ":Gitsigns blame_line<CR>", "Blame For Current Line"},
+    S = { ":Gitsigns stage_hunk<CR>", "Stage Hunk"},
+    u = { ":Gitsigns undo_stage_hunk<CR>", "Undo Stage Hunk"},
+    },
+}, { prefix = "<leader>" })
+
+
+-- ColorScheme keybindings.
+wk.register({
+  t = {
+    name = "Theme",
+    h = { ":Telescope colorscheme<CR>", "Find Colorscheme" },
+    p = { ":Telescope colorscheme enable_preview=true<CR>", "Find Colorscheme with previwer " },
+  }
+}, { prefix = "<leader>" })
+
+
+-- Lsp
+wk.register({
+  l = {
+    name = "LSP",
+    a = { ":lua vim.lsp.buf.code_action()<CR>", "Code Action" },
+    d = { ":Telescope diagnostics<CR>", "Diagnostics" },
+    i = { ":LspInfo<CR>", "Info" },
+    I = { ":LspInstallInfo<CR>", "Installer Info" },
+    r = { ":lua vim.lsp.buf.rename()<CR>", "Rename" },
+    h = { ":lua vim.lsp.buf.hover()<CR>", "Display Information Of Symbol" },
+    s = { ":lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
+    g = {
+      name = "GOTO",
+      D = { ":lua vim.lsp.buf.declaration()<CR>", "Go To Declaration" },
+      i = { ":lua vim.lsp.buf.implementation()<CR>", "Go To Implementation" },
+      d = { "::lua vim.lsp.buf.definition()<CR>", "Go to Definition" },
+      j = { ":lua vim.lsp.diagnostic.goto_prev()<CR>", "Go To Previous Diagnostics" },
+      k = { ":lua vim.lsp.diagnostic.goto_next()<CR>", "Go To Next Diagnostics" },
+      t = { ":lua vim.lsp.buf.type_definition()<CR>", "Go To Type Definition" },
+    },
+    w = {
+      name = "Workspace",
+      l = { ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", "List Workspace Folder" },
+      a = { ":lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Workspace Folder" },
+      r = { ":lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Workspace Folder" },
+    },
+    l = {
+      name = "List Reference/Diagnostic",
+      d = { ":lua vim.lsp.diagnostic.set_loclist()<CR>", "List Diagnostic" },
+      r = { ":lua vim.lsp.buf.references()<CR>", "Show References" },
+    },
+  }
+}, { prefix = "<leader>" })
+
+
+-- Dap
+wk.register({
+  d = {
+    name = "DAP",
+    c = { ":lua require(\"dap\").continue()<CR>", "Continue" },
+    t = { ":lua require(\"dap\").terminate()<CR>", "Terminate" },
+    l = { ":lua require(\"dap\").run_last()<CR>", "Run Last Debugging Config" },
+    d = { ":lua require(\"dap\").repl.open()<CR>", "Open Debug Console" },
+    b = {
+      name = "Breakpoint",
+      t = { ":lua require(\"dap\").toggle_breakpoint()<CR>", "Toggle" },
+      c = { ":lua require(\"dap\").set_breakpoint(vim.fn.input(\"Breakpoint condition: \"))<CR>", "Set conditional" },
+      l = { ":lua require(\"dap\").set_breakpoint(nil, nil, vim.fn.input(\"Log point message: \"))<CR>", "With Log Point Message" },
+    },
+    s = {
+      name = "Step",
+      o = { ":lua require(\"dap\").step_over()<CR>", "Step Over" },
+      O = { ":lua require(\"dap\").step_into()<CR>", "Step Into" },
+      i = { ":lua require(\"dap\").step_out()<CR>", "Step Out" },
+      b = { ":lua require(\"dap\").step_back()<CR>", "Step Back" },
+      c = { ":lua require(\"dap\").run_to_cursor()<CR>", "Run To Cursor" },
+    },
+    u = { ":lua require(\"dapui\").toggle()<CR>", "Toggle UI" },
+  }
+}, { prefix = "<leader>" })
