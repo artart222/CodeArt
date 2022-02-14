9b2b54b19ff786291f7384e17b6a1374980660be
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-02-14 15:31:51 +0330

    New icons have been added

--- configs/lua/plugins/gitsigns.lua
+++ configs/lua/plugins/gitsigns.lua
@@ -9,5 +9,12 @@ require("gitsigns").setup {
   keymaps = {
     noremap = true,
     buffer = true,
-  }
+  },
+  signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
 }
