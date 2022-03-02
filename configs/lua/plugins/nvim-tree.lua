0ef4da5c48267fbc2bf5044dc55aa182e2241361
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-03-02 21:42:28 +0330

    New NvimTree settings have been added

--- configs/lua/plugins/nvim-tree.lua
+++ configs/lua/plugins/nvim-tree.lua
@@ -8,15 +8,10 @@ local tree_cb = require"nvim-tree.config".nvim_tree_callback
 local g = vim.g
 
 g.nvim_tree_auto_ignore_ft = { "dashboard" } -- Don't open tree on specific fiypes.
-g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened.
 g.nvim_tree_indent_markers = 1 -- This option shows indent markers when folders are open.
 g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 0 -- Will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_add_trailing = 0 -- Append a trailing slash to folder names. ]]
-vim.g.nvim_tree_window_picker_exclude = {
-    filetype = { "packer", "vista_kind", "toggleterm" },
-    buftype = { "terminal" }
-}
 
 nvimtree.setup {
   auto_close = false,
@@ -57,6 +52,18 @@ nvimtree.setup {
       list = {
        {key = "<S-h>", cb = ":call ResizeLeft(3)<CR>"},
        {key = "<C-h>", cb = tree_cb("toggle_dotfiles")},
+      }
+    }
+  },
+  open_file = {
+    quit_on_open = false,
+    resize_window = false,
+    window_picker = {
+      enable = true,
+      chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+      exclude = {
+        filetype = { "packer", "vista_kind", "toggleterm" },
+        buftype  = { "terminal" },
       }
     }
   }
