ccc7617d4a7e17b52aafa599ccd620d625cd59d2
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-03-25 04:10:45 +0430

    Format code

--- configs/lua/lsp.lua
+++ configs/lua/lsp.lua
@@ -10,7 +10,7 @@ lsp_installer.on_server_ready(function(server)
   }
   server:setup(
     opts,
-    {capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),}
-    )
+    { capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()) }
+  )
   vim.cmd([[ do User LspAttachBuffers ]])
 end)
