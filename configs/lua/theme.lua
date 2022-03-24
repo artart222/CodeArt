396fbfe5dc6e2d5e8cbba43417788f607098f824
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-03-24 18:27:17 +0430

    Add dynamic diagnostic signs color

--- configs/lua/theme.lua
+++ configs/lua/theme.lua
@@ -2,38 +2,48 @@
 vim.opt.fillchars = { eob = " " }
 
 -- -- Highlightign line number for lsp diagnostics sings
-vim.api.nvim_create_autocmd("VimEnter", {
+vim.api.nvim_create_autocmd({"Colorscheme", "VimEnter"}, {
   pattern = "*",
 
   callback = function()
+    local fn = vim.fn
+    local error_bcolor = fn.synIDattr(fn.synIDtrans(fn.hlID("DiagnosticSignError")), "fg")
+    local info_bcolor = fn.synIDattr(fn.synIDtrans(fn.hlID("DiagnosticSignInfo")), "fg")
+    local warn_bcolor = fn.synIDattr(fn.synIDtrans(fn.hlID("DiagnosticSignWarn")), "fg")
+    local hint_bcolor = fn.synIDattr(fn.synIDtrans(fn.hlID("DiagnosticSignHint")), "fg")
+
+    local error_fcolor = fn.synIDattr(fn.synIDtrans(fn.hlID("DiagnosticSignError")), "bg")
+    local info_fcolor = fn.synIDattr(fn.synIDtrans(fn.hlID("DiagnosticSignInfo")), "bg")
+    local warn_fcolor = fn.synIDattr(fn.synIDtrans(fn.hlID("DiagnosticSignWarn")), "bg")
+    local hint_fcolor = fn.synIDattr(fn.synIDtrans(fn.hlID("DiagnosticSignHint")), "bg")
     vim.api.nvim_set_hl(0, "DiagnosticLineNrError", {
-      bg = "#51202A",
-      fg = "#FF0000",
+      bg = error_bcolor,
+      fg = error_fcolor,
       bold = true,
     })
 
     vim.api.nvim_set_hl(0, "DiagnosticLineNrWarn", {
-      bg = "#51412A",
-      fg = "#FFA500",
+      bg = warn_bcolor,
+      fg = warn_fcolor,
       bold = true,
     })
 
     vim.api.nvim_set_hl(0, "DiagnosticLineNrInfo", {
-      bg = "#1E535D",
-      fg = "#00FFFF",
+      bg = info_bcolor,
+      fg = info_fcolor,
       bold = true,
     })
 
     vim.api.nvim_set_hl(0, "DiagnosticLineNrHint", {
-      bg = "#1E205D",
-      fg = "#000FFF",
+      bg = hint_bcolor,
+      fg = hint_fcolor,
       bold = true,
     })
 
-    vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "", numhl = "DiagnosticLineNrError" })
-    vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "", numhl = "DiagnosticLineNrWarn" })
-    vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "", numhl = "DiagnosticLineNrInfo" })
-    vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "", numhl = "DiagnosticLineNrHint" })
+    fn.sign_define("DiagnosticSignError", { text = "", texthl = "", numhl = "DiagnosticLineNrError" })
+    fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "", numhl = "DiagnosticLineNrWarn" })
+    fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "", numhl = "DiagnosticLineNrInfo" })
+    fn.sign_define("DiagnosticSignHint", { text = "", texthl = "", numhl = "DiagnosticLineNrHint" })
   end,
 })
 
