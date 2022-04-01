d8e7d4b36284e1db9ecdc1685c06079e4bfc2f45
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-04-01 08:21:56 +0430

    Fix some mistakes

--- configs/lua/utils.lua
+++ configs/lua/utils.lua
@@ -2,13 +2,13 @@ local fn = vim.fn
 
 local M = {}
 
-M.plugins_path = fn.stdpath("data") .. "/site/pack/packer/"
+M.plugins_path = fn.stdpath("data") .. "/site/pack/packer"
 M.os = vim.loop.os_uname().sysname
 
 function M.is_plugin_installed(plugins_name)
   if
-    not (fn.empty(fn.glob(M.plugins_path .. "/start/" .. M.plugins_path)) > 0)
-    or not (fn.empty(fn.glob(M.plugins_path .. "/opt/" .. M.plugins_path)) > 0)
+    fn.empty(fn.glob(M.plugins_path .. "/start/" .. plugins_name)) > 0
+    and fn.empty(fn.glob(M.plugins_path .. "/opt/" .. plugins_name)) > 0
   then
     return false
   else
