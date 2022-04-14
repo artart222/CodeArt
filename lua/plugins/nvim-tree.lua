f10682b43c8e495e3047688da2694718715dc74c
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-04-15 01:04:01 +0430

    Add new configs

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,7 +8,6 @@ local tree_cb = require("nvim-tree.config").nvim_tree_callback
 local g = vim.g
 
 g.nvim_tree_auto_ignore_ft = { "alpha" } -- Don't open tree on specific fiypes.
-g.nvim_tree_indent_markers = 1 -- This option shows indent markers when folders are open.
 g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 0 -- Will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_add_trailing = 0 -- Append a trailing slash to folder names. ]]
@@ -44,6 +43,11 @@ local nvimtree_config = {
       "node_modules",
       ".cache",
       "__pycache__",
+    },
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
     },
   },
   view = {
