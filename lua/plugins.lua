44b2f017f97b6679425c77ed97e99ff4fa571fc2
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-04-30 00:39:32 +0430

    User can add snippets

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -304,7 +304,8 @@ return require("packer").startup({
       "L3MON4D3/LuaSnip",
       after = "friendly-snippets",
       config = function()
-        require("luasnip/loaders/from_vscode").load()
+        require("luasnip.loaders.from_vscode").load()
+        require("luasnip.loaders.from_snipmate").lazy_load()
       end,
       disable = disable_plugins.luasnip,
     })
