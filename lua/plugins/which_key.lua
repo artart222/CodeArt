01f57d4d53ff3a08be97cabe3ea44f7d5ea7b608
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-05-20 18:21:13 +0430

    Fix which-key mappings for debugging mappings

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -219,14 +219,18 @@ end
 which_key.register(lsp_maps, { prefix = "<leader>" })
 
 -- Debugger
-local debug_maps = {
-  d = {
-    name = "Debugging",
-    c = { ':lua require("dap").continue()<CR>', "Continue" },
-    t = { ':lua require("dap").terminate()<CR>', "Terminate" },
-    l = { ':lua require("dap").run_last()<CR>', "Run Last Debugging Config" },
-    d = { ':lua require("dap").repl.open()<CR>', "Open Debug Console" },
-    b = {
+if is_plugin_installed("nvim-dap") or is_plugin_installed("nvim-dap-ui") then
+  local debug_maps = {
+    d = {
+      name = "Debugging",
+    },
+  }
+  if is_plugin_installed("nvim-dap") then
+    debug_maps.d.c = { ':lua require("dap").continue()<CR>', "Continue" }
+    debug_maps.d.t = { ':lua require("dap").terminate()<CR>', "Terminate" }
+    debug_maps.d.l = { ':lua require("dap").run_last()<CR>', "Run Last Debugging Config" }
+    debug_maps.d.d = { ':lua require("dap").repl.open()<CR>', "Open Debug Console" }
+    debug_maps.d.b = {
       name = "Breakpoint",
       t = { ':lua require("dap").toggle_breakpoint()<CR>', "Toggle" },
       c = { ':lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', "Set conditional" },
@@ -234,21 +238,21 @@ local debug_maps = {
         ':lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>',
         "With Log Point Message",
       },
-    },
-    s = {
+    }
+    debug_maps.d.s = {
       name = "Step",
       o = { ':lua require("dap").step_over()<CR>', "Step Over" },
       O = { ':lua require("dap").step_into()<CR>', "Step Into" },
       i = { ':lua require("dap").step_out()<CR>', "Step Out" },
       b = { ':lua require("dap").step_back()<CR>', "Step Back" },
       c = { ':lua require("dap").run_to_cursor()<CR>', "Run To Cursor" },
-    },
-  },
-}
-if is_plugin_installed("nvim-dap-ui") then
-  debug_maps.d.u = { ':lua require("dapui").toggle()<CR>', "Toggle UI" }
+    }
+  end
+  if is_plugin_installed("nvim-dap-ui") then
+    debug_maps.d.u = { ':lua require("dapui").toggle()<CR>', "Toggle UI" }
+  end
+  which_key.register(debug_maps, { prefix = "<leader>" })
 end
-which_key.register(debug_maps, { prefix = "<leader>" })
 
 -- Comment
 if is_plugin_installed("kommentary") then
