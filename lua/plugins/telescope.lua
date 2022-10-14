be5bf9155ef2240c1e65c83b2869a30b6a7b61ff
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-10-14 23:18:44 +0330

    Formats code

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,23 +7,21 @@ local previewers = require("telescope.previewers")
 local Job = require("plenary.job")
 local new_maker = function(filepath, bufnr, opts)
   filepath = vim.fn.expand(filepath)
-  Job
-    :new({
-      command = "file",
-      args = { "--mime-type", "-b", filepath },
-      on_exit = function(j)
-        local mime_type = vim.split(j:result()[1], "/")[1]
-        if mime_type == "text" then
-          previewers.buffer_previewer_maker(filepath, bufnr, opts)
-        else
-          -- maybe we want to write something to the buffer here
-          vim.schedule(function()
-            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
-          end)
-        end
-      end,
-    })
-    :sync()
+  Job:new({
+    command = "file",
+    args = { "--mime-type", "-b", filepath },
+    on_exit = function(j)
+      local mime_type = vim.split(j:result()[1], "/")[1]
+      if mime_type == "text" then
+        previewers.buffer_previewer_maker(filepath, bufnr, opts)
+      else
+        -- maybe we want to write something to the buffer here
+        vim.schedule(function()
+          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+        end)
+      end
+    end,
+  }):sync()
 end
 
 local os = require("utils").os
