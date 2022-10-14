6a400132abc60318a3fb4936b042ebec4984871c
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-10-14 23:17:25 +0330

    Modifies settings for 0.8 version of nvim

--- lua/settings.lua
+++ lua/settings.lua
@@ -6,10 +6,6 @@ local exec = vim.api.nvim_exec
 local is_plugin_installed = require("utils").is_plugin_installed
 local autocmd = vim.api.nvim_create_autocmd
 
--- Using new filetype detection system(written in lua).
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
-
 -- Decrease time of completion menu.
 opt.updatetime = 300
 
