e383d868bce3cae6927d47b3b0ab0f3a786df799
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2024-12-06 23:01:16 +0330

    Indentation has been fixed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,34 @@
 local present, nvim_treesitter = pcall(require, "nvim-treesitter.configs")
 if not present then
-    return
+  return
 end
 
 local treesitter_config = {
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-    matchup = {
-        enable = true,
-    },
-    indent = {
-        enable = true,
-        disable = { "python" },
-    },
-    -- autotag = {
-    --   enable = true,
-    -- },
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  matchup = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+    disable = { "python" },
+  },
+  -- autotag = {
+  --   enable = true,
+  -- },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
 }
 
 local config = require("user_settings")
 if config.treesitter then
-    for k, v in pairs(config.treesitter) do
-        treesitter_config[k] = v
-    end
+  for k, v in pairs(config.treesitter) do
+    treesitter_config[k] = v
+  end
 end
 
 nvim_treesitter.setup(treesitter_config)
